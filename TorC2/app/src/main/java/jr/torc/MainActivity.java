2c3323edc4b7b56caeae19cc7997962ccbde1324
Josh R <40114815@live.napier.ac.uk>
2016-03-11 11:08:59 +0000

    button text changes colour on appropriate clicks

--- TorC2/app/src/main/java/jr/torc/MainActivity.java
+++ TorC2/app/src/main/java/jr/torc/MainActivity.java
@@ -1,11 +1,15 @@
 package jr.torc;
 
 import android.content.Context;
+import android.graphics.Color;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 import android.widget.LinearLayout;
 
 
@@ -17,6 +21,26 @@ public class MainActivity extends ActionBarActivity {
         setContentView(R.layout.activity_main);
         LinearLayout mug = (LinearLayout) findViewById(R.id.flPanel);
         mug.addView(new Panel(this), 0);      //adding the panel view to the linear layout flPanel
+
+        final Button btnTea = (Button) findViewById(R.id.btnTea);             //variable for the tea button
+        final Button btnCoffee = (Button) findViewById(R.id.btnCoffee);       //variable used for the coffee button
+        Button btnNext = (Button) findViewById(R.id.btnNextOrder);      //variable for the next person button
+
+        btnTea.setOnClickListener(new View.OnClickListener() {      //opens up new on click listener tea button
+            @Override
+            public void onClick(View v) {
+                btnTea.setTextColor(Color.RED);      //sets the font colour of the button to red
+                btnCoffee.setTextColor(Color.BLACK); //Sets the coffee button's colour back to normal
+            }
+        });
+
+        btnCoffee.setOnClickListener(new View.OnClickListener(){        //opens up a new on click listener for the coffee button
+            @Override
+            public void onClick(View v){
+                btnCoffee.setTextColor(Color.RED);       //changes the buttons colour
+                btnTea.setTextColor(Color.BLACK);       //changes the tea buttons colour back to normal
+            }
+        });
     }
 
     @Override
