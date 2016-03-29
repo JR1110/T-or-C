4d33eb722939ebfceb781677693561536b68dae4
Josh R <40114815@live.napier.ac.uk>
2016-03-29 11:38:28 +0100

    going back to lists

--- TorC2/app/src/main/java/jr/torc/expandedListView.java
+++ TorC2/app/src/main/java/jr/torc/expandedListView.java
@@ -22,11 +22,15 @@ import java.util.List;
 
 public class expandedListView extends BaseExpandableListAdapter {
 
-    private List<String> headings;      //list to store the titles
-    private HashMap<String, List<String>> allOrders;      //hashmap of all orders
+    private String[] headings;      //list to store the titles
+    private String [][] allOrders;      //hashmap of all orders
     private Context context;
 
-    public expandedListView(List<String> Headings, HashMap<String, List<String>> AllOrders, Context Context)
+    private class ViewHolder {
+        TextView text;
+    }
+
+    public expandedListView(String [] Headings, String [][] AllOrders, Context Context)
     {
         this.headings = Headings;       //setting up headings
         this.allOrders = AllOrders;     //setting up all orders
@@ -35,22 +39,22 @@ public class expandedListView extends BaseExpandableListAdapter {
 
     @Override
     public int getGroupCount() {
-        return headings.size();     //makes it only as big as needs be
+        return headings.length;     //makes it only as big as needs be
     }
 
     @Override
     public int getChildrenCount(int groupPosition) {
-        return this.allOrders.get(this.headings.get(groupPosition)).size();
+        return allOrders[groupPosition].length;
     }
 
     @Override
     public Object getGroup(int groupPosition) {
-        return headings.get(groupPosition);
+        return headings[groupPosition];
     }
 
     @Override
     public Object getChild(int groupPosition, int childPosition) {
-        return allOrders.get(headings.get(groupPosition)).get(groupPosition);
+        return allOrders[groupPosition] [childPosition];
     }
 
     @Override
@@ -65,40 +69,46 @@ public class expandedListView extends BaseExpandableListAdapter {
 
     @Override
     public boolean hasStableIds() {
-        return false;
+        return true;
     }
 
     @Override
     public View getGroupView(int groupPosition, boolean isExpanded, View convertView, ViewGroup parent) {
-        String headingTitle = (String) getGroup(groupPosition);
+
+        ViewHolder holder = new ViewHolder();
 
         if (convertView == null) {
-            LayoutInflater infalInflater = (LayoutInflater) this.context.getSystemService(context.LAYOUT_INFLATER_SERVICE);
+            LayoutInflater inf = (LayoutInflater) this.context.getSystemService(context.LAYOUT_INFLATER_SERVICE);
 
-            convertView = infalInflater.inflate(R.layout.groups, null);
+            convertView = inf.inflate(R.layout.groups, parent, false);
+
+
+            holder.text = (TextView) convertView.findViewById(R.id.headings);
+
+            convertView.setTag(holder);
         }
 
-        TextView lblListHeader = (TextView) convertView.findViewById(R.id.headings);
-        lblListHeader.setTypeface(null, Typeface.BOLD);
-        lblListHeader.setText(headingTitle);
+        holder.text.setText(getGroup(groupPosition).toString());
 
         return convertView;
     }
 
     @Override
     public View getChildView(int groupPosition, int childPosition, boolean isLastChild, View convertView, ViewGroup parent) {
-        final String childText = (String) getChild(groupPosition, childPosition);
 
-        LayoutInflater inf = (LayoutInflater) this.context.getSystemService(context.LAYOUT_INFLATER_SERVICE);
+        ViewHolder h = new ViewHolder();
 
         if (convertView == null) {
+            LayoutInflater inf = (LayoutInflater) this.context.getSystemService(context.LAYOUT_INFLATER_SERVICE);
 
-            convertView = inf.inflate(R.layout.items, null);
+            convertView = inf.inflate(R.layout.items, parent, false);
 
+            h.text = (TextView) convertView.findViewById(R.id.child);
+
+            convertView.setTag(h);
         }
 
-        TextView txtChild = (TextView) convertView.findViewById(R.id.child);
-        txtChild.setText(childText);
+        h.text.setText(getChild(groupPosition, childPosition).toString());
 
         return convertView;
     }
